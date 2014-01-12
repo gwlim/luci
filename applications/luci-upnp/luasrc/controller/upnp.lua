1c2fedf32b8dac4a92943570226566411edbe4a4
guowei <limguowei.10@ichat.sp.edu.sg>
2014-01-12 22:20:00 +0800

    Add UPnP Text Change

--- applications/luci-upnp/luasrc/controller/upnp.lua
+++ applications/luci-upnp/luasrc/controller/upnp.lua
@@ -21,7 +21,7 @@ function index()
 
 	local page
 
-	page = entry({"admin", "services", "upnp"}, cbi("upnp/upnp"), _("UPNP"))
+	page = entry({"admin", "services", "upnp"}, cbi("upnp/upnp"), _("UPnP"))
 	page.dependent = true
 
 	entry({"admin", "services", "upnp", "status"}, call("act_status")).leaf = true
