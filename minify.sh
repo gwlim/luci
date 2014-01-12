78da609f20a288db2bc86f491425ff034bef1b52
guowei <limguowei.10@ichat.sp.edu.sg>
2014-01-12 22:54:37 +0800

    Do not clobber

--- minify.sh
+++ minify.sh
@@ -4,14 +4,14 @@ for file in $( find $directory -name '*.js' )
 do
   echo $file
   java -jar compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS --js="$file" --js_output_file="$file-min.js"
-  mv "$file" original_js_css/
-  mv "$file-min.js" "$file"
+  mv -n "$file" original_js_css/
+  mv -n "$file-min.js" "$file"
 done 
 
 for file in $( find $directory -name '*.css' )
 do
   echo $file
   java -jar yuicompressor-2.4.8pre.jar -o "$file-min.css" "$file"
-  mv "$file" original_js_css/
-  mv "$file-min.css" "$file"
+  mv -n "$file" original_js_css/
+  mv -n "$file-min.css" "$file"
 done 
